0081b331c38559cddef7f01fa384c6d9ab3ea156
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-06-21 14:18:05 +0900

    New: config.php | debug => false

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/** op-unit-form:/config.php
+ *
+ * @created    2024-06-21
+ * @version    1.0
+ * @package    op-unit-form
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\FORM;
+
+//	...
+return [
+	'debug' => false,
+];
